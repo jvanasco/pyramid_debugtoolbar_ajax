29cb3d9290dbc87e6904c0c84c25b661f8738f9e
jonathan vanasco <jonathan@2xlp.com>
2017-05-05 19:42:13 -0400

    v0.1.0

--- pyramid_debugtoolbar_ajax/__init__.py
+++ pyramid_debugtoolbar_ajax/__init__.py
@@ -2,6 +2,6 @@ from .panels.ajax import AjaxDebugPanel
 
 
 def includeme(config):
-    config.registry.settings['debugtoolbar.panels'].append(AjaxDebugPanel)
+    config.registry.settings['debugtoolbar.extra_panels'].append(AjaxDebugPanel)
     if 'mako.directories' not in config.registry.settings:
         config.registry.settings['mako.directories'] = []
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author="Jonathan Vanasco",
     author_email="jonathan@findmeon.com",
     url="https://github.com/jvanasco/pyramid_debugtoolbar_ajax",
-    version="0.0.4",
+    version="0.1.0",
     description="Ajax support for pyramid_debugtoolbar",
     keywords="web pyramid",
     license="MIT",
