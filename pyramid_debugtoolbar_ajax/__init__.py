e91903bb1707b88fbdfe10c45612ace7c3eaf7d8
jonathan vanasco <jonathan@2xlp.com>
2019-04-29 12:32:36 -0400

    v0.1.1

--- pyramid_debugtoolbar_ajax/__init__.py
+++ pyramid_debugtoolbar_ajax/__init__.py
@@ -1,6 +1,9 @@
 from .panels.ajax import AjaxDebugPanel
 
 
+__VERSION__ = '0.1.1'
+
+
 def includeme(config):
     config.registry.settings['debugtoolbar.extra_panels'].append(AjaxDebugPanel)
     if 'mako.directories' not in config.registry.settings:
